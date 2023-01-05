131abdbd67d5ed41fbeb85d5bbb6197168fb4de5
Isabella Lopes <icmlopes@gmail.com>
2023-01-05 17:51:29 -0300

    Static  layout of login page with responsivity and form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ResetCss } from "../src/Assets/ResetCss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TimelinePage from "./Pages/TimelinePage";
 import HashTagsPage from "./Pages/HashTagPage";
+import SignInPage from "./Pages/SignInPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/timeline" element={<TimelinePage />} />
           <Route path="/hashtag/:hashtag" element={<HashTagsPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
         </Routes>
       </BrowserRouter>
     </>
