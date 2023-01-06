d1a2f5c089cef6433c6f56e35aa96979586e1eb4
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-01-06 20:52:46 -0300

    refact: delete consoles

--- src/Components/TimelinePage/PostsHashTags.js
+++ src/Components/TimelinePage/PostsHashTags.js
@@ -6,38 +6,6 @@ import { ReactTagify } from "react-tagify";
 import { useNavigate, useParams } from "react-router-dom";
 import PostLink from "./PostLink";
 
-// const postsExample = [
-//   {
-//     userName: "Maluco Random",
-//     userImage:
-//       "https://i.pinimg.com/originals/64/8b/da/648bda8b742f5f713e94f17ff1b49252.jpg",
-//     likesCount: "12",
-//     postDescription:
-//       "Muito #foda dahora esse link aqui rapeize!! #sabadaco #kasino",
-//     linkInfo: {
-//       linkTitle: "Kasino no Sabadaço - AE KASINÃO / KASINAUM",
-//       linkDescription:
-//         'SIGA NO TWITTER: @_YuriRodrigues(Todos os direitos reservados à TV Bandeirantes)Partes de destaque (fora o GC eterno "NA SEQUÊNCIA - Thammy e as revelações n...',
-//       linkUrl:
-//         "https://www.youtube.com/watch?v=umBWV2QC0xo&ab_channel=YuriRodrigues",
-//       linkImage: "https://i.ytimg.com/vi/umBWV2QC0xo/hqdefault.jpg",
-//     },
-//   },
-//   {
-//     userName: "Nerdola Geek",
-//     userImage: "https://cf.shopee.com.br/file/0a4dba0f50d3862f3118154895d987bc",
-//     likesCount: "15",
-//     postDescription: "Mais um dia #curtindo #RPG",
-//     linkInfo: {
-//       linkTitle: "Blog - RPG Next",
-//       linkDescription: "Blog com conteúdo com RPG.",
-//       linkUrl: "https://www.rpgnext.com.br/blog/",
-//       linkImage:
-//         "https://i1.wp.com/www.rpgnext.com.br/wp-content/uploads/2015/09/canyon-BLOG-image.jpg?fit=1920%2C720&ssl=1",
-//     },
-//   },
-// ];
-
 export default function PostHashTags() {
   const navigate = useNavigate();
   const { hashtag } = useParams();
@@ -45,6 +13,7 @@ export default function PostHashTags() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  
   // const token = JSON.parse(localStorage.getItem("token"));
 
   const tagStyle = {
