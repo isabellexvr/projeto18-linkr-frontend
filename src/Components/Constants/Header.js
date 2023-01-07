3827596a11f9fbe4597a21c9936752b77d9a63b8
Isabella Lopes <icmlopes@gmail.com>
2023-01-06 21:55:05 -0300

    feat: decrypting the user's token and changing the linkr logo to writing instead of image

--- src/Components/Constants/Header.js
+++ src/Components/Constants/Header.js
@@ -1,71 +1,68 @@
 import styled from "styled-components";
-import logo from "../../Assets/linkr.png";
 import { AiOutlineDown } from "react-icons/ai";
 import Searchbox from "./Searchbox";
 import useWindowDimensions from "../Services/windowDimensions";
+import { AuthContext } from "../../Components/Context/authContext";
+import jwtDecode from "jwt-decode";
+import { useContext } from "react";
 
 export default function Header() {
-	const { width } = useWindowDimensions();
-	return (
-		<HeaderStyle>
-			<Container>
-				<div className='logo'>
-					<Logo
-						alt='logo'
-						src={logo}
-					/>
-				</div>
-				{width >= 840 && <Searchbox />}
-				<UserContainer>
-					<AiOutlineDown />
-					<UserProfilePicture
-						alt='user-profile'
-						src='https://i.pinimg.com/originals/64/8b/da/648bda8b742f5f713e94f17ff1b49252.jpg'
-					/>
-				</UserContainer>
-			</Container>
-		</HeaderStyle>
-	);
+  const { width } = useWindowDimensions();
+  const { token } = useContext(AuthContext);
+
+  const payload = jwtDecode(token);
+
+  return (
+    <HeaderStyle>
+      <Container>
+        <Logo>linkr</Logo>
+        {width >= 840 && <Searchbox />}
+        <UserContainer>
+          <AiOutlineDown />
+          <UserProfilePicture alt="user-profile" src={payload.userPicture} />
+        </UserContainer>
+      </Container>
+    </HeaderStyle>
+  );
 }
 
 const HeaderStyle = styled.div`
-	height: 72px;
-	background: #151515;
-	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	/* align-items: center; */
-	position: fixed;
-	top: 0;
-	left: 0;
+  height: 72px;
+  background: #151515;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  position: fixed;
+  top: 0;
+  left: 0;
 `;
 
 const Container = styled.div`
-	width: 91.7%;
-	display: flex;
-	justify-content: space-between;
-	.logo {
-		display: flex;
-		align-items: center;
-	}
+  width: 91.7%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const UserContainer = styled.div`
-	display: flex;
-	align-items: center;
-	> svg {
-		color: white;
-		font-size: 28px;
-		margin-right: 12px;
-	}
+  display: flex;
+  align-items: center;
+  > svg {
+    color: white;
+    font-size: 28px;
+    margin-right: 12px;
+  }
 `;
 
 const UserProfilePicture = styled.img`
-	width: 44px;
-	border-radius: 50%;
+  width: 44px;
+  border-radius: 50%;
 `;
-const Logo = styled.img`
-	width: initial;
-	height: initial;
+const Logo = styled.div`
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 50px;
+  font-family: "Passion One";
 `;
